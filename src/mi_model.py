7ca33bcb1c34e3b5a82bf7bc6e0685d962512dc3
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-08-17 21:52:46 -0400

    bug fixes to recurrent mode

--- src/mi_model.py
+++ src/mi_model.py
@@ -4,24 +4,14 @@
 #################################
 #################################
 ####### IMPORTS #################
-from pprint import pprint
-
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import (
-#     f1_score, recall_score, precision_score, roc_auc_score,
-#     mean_squared_error, mean_absolute_error, r2_score
-# )
-# from scipy.stats import pearsonr
-
-import numpy as np
 import torch
 import torch.nn as nn
 #################################
 
 #################################
 ####### VARIABLES ###############
+# TODO: Move this to the main script
 torch.manual_seed(42)
-np.random.seed(42)
 #################################
 
 class recurrent(nn.Module):
@@ -35,30 +25,33 @@ class recurrent(nn.Module):
         self.dropout = dropout
         self.num_classes = num_classes
         
+        # TODO: Positional encoding
         self.model = []
         
         self.model.append(nn.GRU(input_size = self.input_size, hidden_size = self.hidden_size, num_layers = self.num_layers, \
                                  dropout = self.dropout, bidirectional = self.bidirectional, batch_first=True))
         
         multiplier = (bidirectional + 1)
-        self.model.append(nn.Linear(self.hidden_size*multiplier, self.num_classes))
+        if self.num_classes<=2:
+            self.model.append(nn.Linear(self.hidden_size*multiplier, 1))
+        else:
+            self.model.append(nn.Linear(self.hidden_size*multiplier, self.num_classes))
         
         self.model = nn.ModuleList(self.model)
         
         
-    def forward(self, input_rep, hidden_rep=None, mask=None, predict_last_valid_hidden_state=True):
+    def forward(self, embeddings:torch.Tensor, hidden_rep:torch.Tensor=None, mask:torch.Tensor=None, predict_last_valid_hidden_state:bool=True):
         """
-            input_rep: (batch_size, seq_len, input_size)
+            embeddings: (batch_size, seq_len, input_size)
             hidden_rep: (num_layers * num_directions, batch_size, hidden_size)
             mask: (batch_size, seq_len) of type bool. 1 if the token is valid, 0 if not.
             predict_last_valid_hidden_state: If True, then the last valid timestep's hidden state from each instance is used for prediction. Else, all timesteps' hidden states are predicted.
         """
-        # TODO: Check if the masking works correctly to perform MI prediction
         if mask is not None:
-            # assert isinstance(mask, torch.BoolTensor), "Mask should be of type BoolTensor"
-            assert mask.shape == torch.Size(input_rep.shape[:2]), "Mask shape should be (batch_size, seq_len)"
+            # assert isinstance(mask, torch.BoolTensor), "Mask should be of type BoolTensor" #TODO: This line throws assertion error although the mask is of type BoolTensor
+            assert mask.shape == torch.Size(embeddings.shape[:2]), "Mask shape should be (batch_size, seq_len)"
         
-        output_rep = input_rep
+        output_rep = embeddings
         for layer in self.model:
             
             if mask is not None:
@@ -77,9 +70,9 @@ class recurrent(nn.Module):
                     pos_mask[torch.arange(output_rep.shape[0]), idx] = 1
                     output_rep = (output_rep*pos_mask.unsqueeze(-1)).sum(dim=1)
                 else: # Predict for all timesteps' hidden states; Zero out the hidden states using mask if available
-                    if mask is not None:
-                        output_rep = (output_rep*mask.unsqueeze(-1))
-                output = layer(output_rep)
+                        if mask is not None: output_rep = (output_rep*mask.unsqueeze(-1))  
+                output = layer(output_rep).squeeze(-1)
+                
         
         return output
 #################################
