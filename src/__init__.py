5a3165f2919ef60ce401ae596ad5fba04cef1b12
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-08-18 00:32:02 -0400

    changes to init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .mi_datamodule import get_dataset, create_mask, default_collate_fn, MIDataLoaderModule
+from .mi_datamodule import get_datasetDict, create_mask, default_collate_fn, MIDataLoaderModule
 from .mi_model import recurrent
 from .mi_lightningmodule import MILightningModule
 from .mi_args import get_data_args, get_model_args, get_training_args, get_default_args
