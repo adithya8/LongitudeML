95d807ed94239723a1d8a47d795bee875375cb42
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2023-08-24 09:26:23 -0400

    added get logger fn to init

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,6 @@ from .mi_datamodule import get_datasetDict, create_mask, default_collate_fn, MID
 from .mi_model import recurrent
 from .mi_lightningmodule import MILightningModule
 from .mi_args import get_data_args, get_model_args, get_training_args, get_default_args
+
+from .mi_utils import get_logger
 ## TODO: Think about the eval script and functions
