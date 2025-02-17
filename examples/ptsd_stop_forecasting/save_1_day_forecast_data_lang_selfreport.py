817388ad14c83202920458a99b02480d6603ba62
adithya8 <v.adithyaganesan@gmail.com>
2025-02-17 14:49:20 -0500

    added z scoring the subscales data

--- examples/ptsd_stop_forecasting/save_1_day_forecast_data_lang_selfreport.py
+++ examples/ptsd_stop_forecasting/save_1_day_forecast_data_lang_selfreport.py
@@ -56,10 +56,10 @@ def create_outcomes_mask(instance, outcomes_names:List[str]):
     sorted_time_ids = sorted(instance['time_ids'])
     # missing_time_ids = set(range(sorted_time_ids[0], sorted_time_ids[-1]+1)) - set(sorted_time_ids)
     missing_time_ids = set(range(0, sorted_time_ids[-1]+1)) - set(sorted_time_ids)
-    min_time_id = 0 #sorted_time_ids[0]
+    min_time_id = sorted_time_ids[0]
     infill_mask = []  
     # for time_id in range(sorted_time_ids[0], sorted_time_ids[-1]+1):
-    for time_id in range(0, sorted_time_ids[-1]+1):
+    for time_id in range(min_time_id, sorted_time_ids[-1]+1):
         if time_id in missing_time_ids:
             infill_mask.insert(time_id-min_time_id, [0]*num_outcomes) # inserting 0 as mask at the missing time_id
             instance['outcomes'].insert(time_id-min_time_id, [0]*num_outcomes)
@@ -76,11 +76,12 @@ def create_outcomes_mask(instance, outcomes_names:List[str]):
     return instance
 
 
-def merge_features_outcomes(long_outcomes:Dataset, long_features:Dataset):
+def merge_features_outcomes(long_outcomes:Dataset, long_features:Dataset, reset_time2zero:bool=False):
     """
         Merges the features and outcomes datasets.
         1. Takes the min of time_ids between features and outcomes, max of time_ids between features and outcomes.
         2. Merges the datasets based on mutual time_ids, infills the missing time_ids in respective modalities with appropriate values.
+        3. If reset_time2zero is True, then the min time_id is set to 0 for each sequence.
     """
     
     # Find the name of the embeddings features in the long_features dataset
@@ -91,6 +92,7 @@ def merge_features_outcomes(long_outcomes:Dataset, long_features:Dataset):
     merged_dataset = {key: [] for key in long_features.features.keys()}
     merged_dataset['outcomes'] = []
     merged_dataset['outcomes_mask'] = []
+    if reset_time2zero: merged_dataset['orig_time_ids'] = []
     
     def unionize_time_ids(outcomes_instance, features_instance):
         """
@@ -124,6 +126,9 @@ def merge_features_outcomes(long_outcomes:Dataset, long_features:Dataset):
                 merged_instance["outcomes"].append(outcomes_instance["outcomes"][outcomes_time_idx])
                 merged_instance["outcomes_mask"].append(outcomes_instance["outcomes_mask"][outcomes_time_idx])        
         merged_instance["time_ids"] = list(range(min(union_time_ids), max(union_time_ids)+1))
+        if reset_time2zero:
+            merged_instance["orig_time_ids"] = merged_instance["time_ids"]
+            merged_instance["time_ids"] = list(range(0, len(merged_instance["time_ids"])))
         return merged_instance
     
     mutual_seq_ids = set(long_features['seq_id']) & set(long_outcomes['seq_id'])
@@ -437,13 +442,14 @@ if __name__ == '__main__':
     long_outcomes = long_outcomes.map(lambda x: create_outcomes_mask(x, outcomes_names))
     print ("Outcomes mask pattern created.")
 
-    long_merged_features = merge_features([long_sr_features, long_lang_features, long_wtc_pclsubscales], ['_subscales', '_lang', '_wtcSubscales'])
+    long_merged_features = merge_features(feature_datasets=[long_sr_features, long_lang_features, long_wtc_pclsubscales], 
+                                          feature_suffixes=['_subscales', '_lang', '_wtcSubscales'])
     
     # long_comb_features = concatenate_features(long_sr_features, long_lang_features)
     
-    merged_dataset = merge_features_outcomes(long_outcomes, long_merged_features)
+    merged_dataset = merge_features_outcomes(long_outcomes=long_outcomes, long_features=long_merged_features,
+                                             reset_time2zero=True)
     print ("Datasets merged.")
-   
     # Truncate all sequences to 90 time_ids of records, ie., the maximum value of time_id should be 89
     def truncate_sequences(instance, max_time_id):
         time_ids = instance['time_ids']
@@ -467,6 +473,13 @@ if __name__ == '__main__':
     
     merged_dataset_fulllength = merged_dataset_fulllength.filter(filter_sequences)
     
+    # Filter sequences that don't have enough outcomes within train time and eval time ids. Threshold = 50% of the days in train and dev set should have outcomes 
+    def outcomes_filter_sequences(instance):
+        outcomes_mask = [mask[0] for mask in instance['outcomes_mask']]
+        return sum(outcomes_mask[:60]) >= 30 and sum(outcomes_mask[60:]) >= 15 and sum(outcomes_mask[:40]) >= 20 and sum(outcomes_mask[40:60]) >= 10
+    
+    merged_dataset_fulllength = merged_dataset_fulllength.filter(outcomes_filter_sequences)
+        
     # Split the dataset into train, val
     # Stratify based on mean the stddev of the first outcome.
     # Calculate mean and std dev for each seq_id over first outcome when the mask is 1
@@ -492,7 +505,7 @@ if __name__ == '__main__':
     merged_dataset_fulllength = merged_dataset_fulllength.sort('std_outcome').sort('avg_outcome')
     merged_dataset_fulllength = merged_dataset_fulllength.map(stratify_sequences, batched=True, batch_size=num_folds, remove_columns=['avg_outcome', 'std_outcome'])
     
-    # TODO: Create a function that would add another key:value pair to this dataset object maintaining a list for each sequence indicating whether a time_id belongs to train or eval. 
+    # Create a function that would add another key:value pair to this dataset object maintaining a list for each sequence indicating whether a time_id belongs to train or eval. 
     # The function definition: def create_train_eval_time_mask(instance, train_time_ids: List[int], eval_time_ids: List[int]=None) -> dict:
     # Key = "oots_mask"; This value will be 0 for train time_ids and 1 for eval time_ids.
     # Figure the max applicable time_id for each sequence.
@@ -504,22 +517,42 @@ if __name__ == '__main__':
         return instance
     
     train_time_ids = list(range(0, 59))
-    merged_dataset_fulllength = merged_dataset_fulllength.map(lambda x: create_oots_mask(x, train_time_ids))
+    merged_dataset_fulllength = merged_dataset_fulllength.map(lambda x: create_oots_mask(x, train_time_ids))    
     
-    # Filter sequences that don't have enough outcomes within train time and eval time ids. Threshold = 50% of the days in train and dev set should have outcomes 
-    def outcomes_filter_sequences(instance):
-        outcomes_mask = [mask[0] for mask in instance['outcomes_mask']]
-        return sum(outcomes_mask[:60]) >= 30 and sum(outcomes_mask[60:]) >= 15 and sum(outcomes_mask[:40]) >= 20 and sum(outcomes_mask[40:60]) >= 10
+    # Compute the average and std dev of the subscales on the within sample within time set for each sequence 
+    # Compute the average of the average and average of the std dev. 
+    # Use this average of average and average of std dev to normalize all the sequence's subscale values across the entire dataset
     
-    merged_dataset_fulllength = merged_dataset_fulllength.filter(outcomes_filter_sequences)
+    def compute_mean_std_subscales(instance):
+        subscales = [subscale for subscale, mask, oots in zip(instance['embeddings_subscales'], instance['mask_subscales'], instance['oots_mask']) if mask==0 and oots==0]
+        mean_subscales = np.mean(subscales, axis=0).tolist()
+        std_subscales = np.std(subscales, axis=0).tolist()
+        instance['avg_subscales'] = mean_subscales
+        instance['std_subscales'] = std_subscales
+        return instance
+    
+    def normalize_subscales(instance, avg, std):
+        avg, std = np.array(avg), np.array(std)
+        temp = []
+        for idx, (mask, subscales) in enumerate(zip(instance['mask_subscales'], instance['embeddings_subscales'])):
+            ss_z = ((np.array(subscales) - avg) / std).tolist() if mask==0 else [0]*len(subscales)
+            temp.append(ss_z)
+        instance['embeddings_subscales_z'] = temp
+        return instance
+
+    merged_dataset_trainset = merged_dataset_fulllength.filter(lambda x: x['folds'] != 4).map(compute_mean_std_subscales)
+    avg_avg_subscales, avg_std_subscales = np.mean(merged_dataset_trainset['avg_subscales'], axis=0), np.mean(merged_dataset_trainset['std_subscales'], axis=0)
+    merged_dataset_fulllength = merged_dataset_fulllength.map(lambda x: normalize_subscales(x, avg_avg_subscales, avg_std_subscales))
+
     # merged_dataset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/todayPCL_selfreport_PCL_1_days_ahead_max90days_v3_40combined_5fold')
     # merged_dataset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_roberta_base_L11_rpca64_combined_PCL_1_days_ahead_max90days_v4_40combined_5fold')
     # merged_dataset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_noNULLs_roberta_base_L11_rpca64_merged_PCL_1_days_ahead_max90days_v4_40combined_5fold')
     # merged_dataset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_noNULLs_roberta_base_L11_rpca64_hypLexNormalized_merged_PCL_1_days_ahead_max60days_v4_40combined_5fold')
     # merged_dataset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_noNULLs_roberta_base_L11_rpca64_hypLexNormalized_wtcSubscalesNormalized_merged_PCL_1_days_ahead_max60days_v4_40combined_5fold_oots')
     print (merged_dataset_fulllength)
-    merged_dataset_fulllength.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_max90days_v6_60combined_5fold_oots')
-    
+    # merged_dataset_fulllength.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_max90days_v6_60combined_5fold_oots')
+    merged_dataset_fulllength.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreportZ_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_reset_time2zero_max90days_v6_60combined_5fold_oots')
+
     ###############################
     #  Now we create a dev set out of this, by cutting off one fold as the held out sequence and cutting off time points after 60 days as held out time 
     
@@ -535,6 +568,13 @@ if __name__ == '__main__':
         return sum(outcomes_mask[:40]) >= 20 and sum(outcomes_mask[40:]) >= 10
     
     merged_dataset_devset = merged_dataset_devset.filter(outcomes_filter_sequences_dev)
-    merged_dataset_devset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_max60days_v6_40combined_devset_oots')
+    # merged_dataset_devset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreport_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_max60days_v6_40combined_devset_oots')
+    
+    merged_dataset_devset_train = merged_dataset_devset.map(compute_mean_std_subscales)
+    avg_avg_subscales, avg_std_subscales = np.mean(merged_dataset_devset_train['avg_subscales'], axis=0), np.mean(merged_dataset_devset_train['std_subscales'], axis=0)
+    merged_dataset_devset = merged_dataset_devset.map(lambda x: normalize_subscales(x, avg_avg_subscales, avg_std_subscales))
+    
     print (merged_dataset_devset)
+    merged_dataset_devset.save_to_disk('/cronus_data/avirinchipur/ptsd_stop/forecasting/datasets/PCLsubscales_selfreportZ_roberta_laL23rpca64_wtcSubscalesNormalized_merged_PCL_1_days_ahead_reset_time2zero_max60days_v6_40combined_devset_oots')
+
     ###############################    
