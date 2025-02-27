a28733fd5fdcb149269742d17fa5d6b1aca9ad2d
adithya8 <v.adithyaganesan@gmail.com>
2025-02-27 11:11:10 -0500

    added a few baseline models using the ARRidge model

--- examples/ptsd_stop_forecasting/BaselineModels.py
+++ examples/ptsd_stop_forecasting/BaselineModels.py
@@ -1,5 +1,10 @@
-import torch
+from utils import add_to_path
+add_to_path(__file__)
+
 import torch.nn as nn
+import torch
+
+from src import AutoRegressiveLinear
 
 # NOTE: Add the class to the BSLN_ARCHS dictionary at the bottom of the file.
 
@@ -36,9 +41,24 @@ class LinearRegressionSubscales(nn.Module):
     def __init__(self, input_size, output_size):
         super(LinearRegressionSubscales, self).__init__()
         self.linear = nn.Linear(input_size, output_size)
+        self.linear.weight.data.fill_(0.0)
+        self.linear.bias.data.fill_(0.0)
         
     def forward(self, embeddings_subscales, mask_subscales, **kwargs):
         return self.linear(embeddings_subscales)
+
+
+class LinearRegressionSubscalesLang(nn.Module):
+    """ Linear Regression model for Subscales with Language concatenated """
+    def __init__(self, subscales_linear, lang_linear):
+        super(LinearRegressionSubscalesLang, self).__init__()
+        self.subscales_linear = subscales_linear
+        self.lang_linear = lang_linear
+    
+    def forward(self, embeddings_subscales, mask_subscales, embeddings_lang, mask_lang, **kwargs):
+        output = self.subscales_linear(embeddings_subscales, mask_subscales) + self.lang_linear(embeddings_lang, mask_lang)
+        output /= 2.0
+        return output
 
 
 class LinearRegressionSubscalesBN(nn.Module):
@@ -69,6 +89,53 @@ class LinearRegressionSubscalesZ(nn.Module):
     def forward(self, embeddings_subscales_z, mask_subscales, **kwargs):
         return self.linear(embeddings_subscales_z) 
 
+
+class ARSubscale(nn.Module):
+    """Auto Regressive Subscale Model with given history length"""
+    def __init__(self, subscaleAR:AutoRegressiveLinear):
+        super(ARSubscale, self).__init__()
+        self.subscaleAR_model = subscaleAR
+        
+    def forward(self, embeddings_subscales, mask_subscales, **kwargs):
+        return self.subscaleAR_model(embeddings_subscales, mask_subscales, **kwargs)
+
+
+class ARSubscaleZ(nn.Module):
+    """Auto Regressive Subscale Model with Z-scores"""
+    def __init__(self, subscaleAR:AutoRegressiveLinear):
+        super(ARSubscaleZ, self).__init__()
+        self.subscaleAR_model = subscaleAR
+        
+    def forward(self, embeddings_subscales_z, mask_subscales, **kwargs):
+        return self.subscaleAR_model(embeddings_subscales_z, mask_subscales, **kwargs)
+    
+
+class ARSubscaleShifted(nn.Module):
+    """Auto Regressive Subscale Model with shifted embeddings"""
+    def __init__(self, subscaleAR:AutoRegressiveLinear):
+        super(ARSubscaleShifted, self).__init__()
+        self.subscaleAR_model = subscaleAR
+        
+    def forward(self, embeddings_subscales, mask_subscales, **kwargs):
+        embeddings_subscales_shifted = torch.cat([torch.zeros_like(embeddings_subscales[:, :1]), embeddings_subscales[:, :-1]], dim=1)
+        embeddings_subscales_diff = embeddings_subscales - embeddings_subscales_shifted
+        diff_pred = self.subscaleAR_model(embeddings_subscales_diff, mask_subscales, **kwargs)
+        pred = embeddings_subscales_shifted[:, :, :1] + diff_pred  
+        # import pdb; pdb.set_trace()
+        return pred
+
+class ARSubscaleLang(nn.Module):
+    """Auto Regressive Subscale with Language concatenated"""
+    def __init__(self, subscaleAR:AutoRegressiveLinear, LangAR:AutoRegressiveLinear):
+        super(ARSubscaleLang, self).__init__()
+        self.subscaleAR_model = subscaleAR
+        self.langAR_model = LangAR
+        
+    def forward(self, embeddings_subscales, mask_subscales, embeddings_lang, mask_lang, **kwargs):
+        output = self.subscaleAR_model(embeddings_subscales, mask_subscales, **kwargs) + self.langAR_model(embeddings_lang, mask_lang, **kwargs)
+        output /= 2.0
+        return output
+    
 ##############################################
     
 BSLN_ARCHS = {
@@ -76,5 +143,10 @@ BSLN_ARCHS = {
     'linear_ln': LinearRegressionWithLN,
     'linear_subscales': LinearRegressionSubscales,
     'linear_subscales_bn': LinearRegressionSubscalesBN,
-    'linear_subscales_z': LinearRegressionSubscalesZ
+    'linear_subscales_lang': LinearRegressionSubscalesLang,
+    'linear_subscales_z': LinearRegressionSubscalesZ,
+    'ar_subscale': ARSubscale,
+    'ar_subscale_z': ARSubscaleZ,
+    'ar_subscale_shifted': ARSubscaleShifted,
+    'ar_subscale_lang': ARSubscaleLang
 }
